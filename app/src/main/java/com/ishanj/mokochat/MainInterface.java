694694fee314cbe91104dca63fa3ad396cf78d8e
Ishan Jayasinghe <ishanrashithajayasinghe@gmail.com>
2023-06-11 07:33:27 +0530

    Found a sustainable way to retrive data from firebase database and show in app using for loop

--- app/src/main/java/com/ishanj/mokochat/MainInterface.java
+++ app/src/main/java/com/ishanj/mokochat/MainInterface.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
@@ -44,8 +49,62 @@ public class MainInterface extends AppCompatActivity {
         elementInitialize();
         //This triggers main actions
         actionTriggers();
+        //Fetch Sample data
+        fetchList();
 
 
+    }
+
+    private void fetchList() {
+        DatabaseReference listRef = FBdatabase.getReference("texts");
+        // Retrieve the data from Firebase Realtime Database
+        listRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                LinearLayout linearLayout = findViewById(R.id.linearLayout); // Replace with your actual layout container
+                linearLayout.removeAllViews(); // Clear the existing views
+
+                TextView originalTextView = findViewById(R.id.itemTextView);
+
+                int idCounter=1;
+                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                    // Process each child snapshot and retrieve the data
+                    String value = childSnapshot.getValue(String.class);
+
+//                    // Create a new TextView for each item
+//                    TextView textView = new TextView(MainInterface.this); // Replace MainActivity with your activity or use 'getContext()' in a fragment
+//                    textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+//                    textView.setText(value);
+//
+//                    // Add the TextView to the layout container
+//                    linearLayout.addView(textView);
+                    TextView textViewCopy = new TextView(MainInterface.this); // Replace MainActivity with your activity or use 'getContext()' in a fragment
+                    textViewCopy.setLayoutParams(originalTextView.getLayoutParams()); // Set the layout params of the original TextView
+                    textViewCopy.setTextSize(TypedValue.COMPLEX_UNIT_PX, originalTextView.getTextSize());
+                    textViewCopy.setText(value);
+
+                    textViewCopy.setId(idCounter); // Assign a unique ID to the TextView
+                    textViewCopy.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            int textViewId = v.getId();
+                            // Handle the click event and show the ID
+                            Toast.makeText(MainInterface.this, "Clicked TextView ID: " + textViewId, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    // Add the copied TextView to the layout container
+                    linearLayout.addView(textViewCopy);
+
+                    idCounter++; // Increment the ID counter
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("Firebase", "Error fetching data", databaseError.toException());
+            }
+        });
     }
 
     private void newUserCheck() {
